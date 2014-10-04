41e46e1115bd6c6a87a2517e578e1e190f4956dd
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2014-10-04 19:17:23 -0400

    Hardcode the quizzes

---
+++ src/com/example/quizzesuscis/Quiz.java
@@ -0,0 +1,15 @@
+package com.example.quizzesuscis;
+
+public class Quiz
+{
+    private String question;
+    private String answer;
+    
+    /** CONSTRUCTOR */
+    public Quiz(String question, String answer)
+    {
+        this.question = question;
+        this.answer = answer;
+    }
+    
+}
